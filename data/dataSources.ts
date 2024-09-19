85cad35378b5ee651182b3701627c67d219e5f96
artahian <artahian@gmail.com>
2024-09-19 16:36:33 -0700

    Send data model information to cloud backend

--- data/dataSources.ts
+++ data/dataSources.ts
@@ -1,9 +1,9 @@
 import { DataSource } from './types';
 import { loadModules } from '../load';
 
-export async function loadModels() {
-  const dataSources: Record<string, DataSource<any>> = {};
-  
+export const dataSources: Record<string, DataSource<any>> = {};
+
+export async function loadModels() {  
   const modules = await loadModules('**/*model.{js,ts}');
   for (const dataSource of modules) {
     dataSources[dataSource.collectionName] = dataSource;
