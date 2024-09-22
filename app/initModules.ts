bcd5ba353c58b572979a49eb5ae1fe6dff5dfcf3
artahian <artahian@gmail.com>
2024-09-22 15:47:02 -0700

    Receive MongoDB URI from Modelence Hub

--- app/initModules.ts
+++ app/initModules.ts
@@ -1,21 +1,8 @@
 import { loadModels } from '../data/dataSources';
 import { loadModules } from '../load';
-import { connect } from '../db/client';
-import { DataSource } from '../data/types';
-import { addDataSource } from '../db';
 
 export async function initModules() {
-  const dataSources = await loadModels();
+  await loadModels();
   await loadModules('**/*(.actions|actions).{js,ts}');
   await loadModules('**/*(.loaders|loaders).{js,ts}');
-
-  const client = await connect();
-
-  Object.values(dataSources).forEach((dataSource: DataSource<any>) => {
-    addDataSource(dataSource);
-    dataSource.indexes.forEach((index) => {
-      const collection = client.db().collection(dataSource.collectionName);
-      collection.createIndex(index);
-    });
-  });
 }
--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,18 @@
 import { DataSource } from '../data/types';
 import { MongoCollection } from './MongoCollection';
-import { getClient } from './client';
+import { connect } from './client';
+import { dataSources } from '../data/dataSources';
 
 export const db: Record<string, MongoCollection> = {};
 
-export function addDataSource(dataSource: DataSource<any>) {
-  const client = getClient();
-  if (!client) {
-    throw new Error('Mongo client is not initialized');
-  }
-  const collection = client.db().collection(dataSource.collectionName);
-  db[dataSource.collectionName] = new MongoCollection(collection);
+export async function initDb(mongodbUri: string) {
+  const client = await connect(mongodbUri);
+
+  Object.values(dataSources).forEach((dataSource: DataSource<any>) => {
+    const collection = client.db().collection(dataSource.collectionName);
+    db[dataSource.collectionName] = new MongoCollection(collection);
+    dataSource.indexes.forEach((index) => {
+      collection.createIndex(index);
+    });
+  });
 }
