cc606f633a7a28228f266125ffdd7328a1663cad
artahian <artahian@gmail.com>
2024-09-29 14:00:01 -0700

    Add config validation for init and _system.

--- config/server.ts
+++ config/server.ts
@@ -2,12 +2,17 @@ import { ConfigKey, AppConfig, ConfigSchema } from './types';
 
 let configSchema: ConfigSchema = {};
 let config: Record<ConfigKey, AppConfig> = {};
+let isInitialized = false;
 
 export function getConfig(key: ConfigKey) {
   return config[key];
 }
 
 export function getPublicConfigs() {
+  if (!isInitialized) {
+    throw new Error('Config is not initialized: an attempt was made to access configs before they were loaded');
+  }
+
   return Object.fromEntries(
     Object.entries(config).filter(([key]) => configSchema[key]?.isPublic)
   );
@@ -26,12 +31,19 @@ export function loadConfigs(configs: AppConfig[]) {
       value
     };
   });
+
+  isInitialized = true;
 }
 
 export function setSchema(schema: ConfigSchema) {
   // TODO: more validation on the schema structure
   Object.entries(schema).forEach(([key, value]) => {
     const { type, isPublic } = value;
+
+    if (key.toLowerCase().startsWith('_system.')) {
+      throw new Error(`Config key cannot start with a reserved prefix: '_system.' (${key})`);
+    }
+
     if (type === 'secret' && isPublic) {
       throw new Error(`Config ${key} with type "secret" cannot be public`);
     }
