7c27b31fb566953240bbb304daa605d6b3d211b1
artahian <artahian@gmail.com>
2024-11-04 21:47:45 -0800

    Initial working basic version of cron

--- db/MongoCollection.ts
+++ db/MongoCollection.ts
@@ -1,4 +1,4 @@
-import { Collection, Document, UpdateResult, DeleteResult, InsertOneResult, AggregateOptions, AggregationCursor } from 'mongodb';
+import { Collection, Document, UpdateResult, DeleteResult, InsertOneResult, AggregateOptions, AggregationCursor, FindCursor } from 'mongodb';
 
 export class MongoCollection {
   private collection: Collection;
@@ -11,11 +11,16 @@ export class MongoCollection {
     return await this.collection.findOne(query, options);
   }
 
-  async find(query: Document, options?: { sort?: Document }): Promise<Document[]> {
+  find(query: Document, options?: { sort?: Document }): FindCursor<Document> {
     const cursor = this.collection.find(query);
     if (options?.sort) {
       cursor.sort(options.sort);
     }
+    return cursor;
+  }
+
+  async fetch(query: Document, options?: { sort?: Document }): Promise<Document[]> {
+    const cursor = this.find(query, options)
     return await cursor.toArray();
   }
 
@@ -27,8 +32,16 @@ export class MongoCollection {
     return await this.collection.updateOne(selector, update);
   }
 
+  async updateMany(selector: Document, update: Document): Promise<UpdateResult> {
+    return await this.collection.updateMany(selector, update);
+  }
+
   async deleteOne(selector: Document): Promise<DeleteResult> {
     return await this.collection.deleteOne(selector);
+  }
+
+  async deleteMany(selector: Document): Promise<DeleteResult> {
+    return await this.collection.deleteMany(selector);
   }
 
   aggregate(pipeline: Document[], options?: AggregateOptions): AggregationCursor<Document> {
