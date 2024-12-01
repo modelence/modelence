a1db28a5ff2882c62367a666128a476b82a6a2fd
artahian <artahian@gmail.com>
2024-12-01 14:29:27 -0800

    Add vite-plugin-eslint

--- vite.ts
+++ vite.ts
@@ -1,22 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 import path from 'path';
+import fs from 'fs';
 
-export const defineViteConfig = () => defineConfig({
-  plugins: [react()],
-  root: process.cwd(),
-  build: {
-    outDir: '.modelence/client',
-    emptyOutDir: true
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'src')
-    }
+export function defineViteConfig() {
+  const appDir = process.cwd();
+
+  const eslintConfigFile = [
+    '.eslintrc.js',
+    '.eslintrc.json',
+    '.eslintrc',
+    'eslint.config.js',
+    '.eslintrc.yml',
+    '.eslintrc.yaml'
+  ].find(file => fs.existsSync(path.join(appDir, file)));
+
+  const plugins = [react()];
+
+  if (eslintConfigFile) {
+    plugins.push(
+      eslint({
+        failOnError: false,
+        include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+        cwd: appDir,
+        overrideConfigFile: path.resolve(appDir, eslintConfigFile)
+      })
+    );
   }
-})
+
+  return defineConfig({
+    plugins,
+    root: appDir,
+    build: {
+      outDir: '.modelence/client',
+      emptyOutDir: true
+    },
+    server: {
+      proxy: {
+        '/api': 'http://localhost:4000'
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(appDir, 'src')
+      }
+
+    }
+  })
+}
+
