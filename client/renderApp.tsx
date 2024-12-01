70a40aba1a1d9c9b7e85c0fac4207b29c0252525
artahian <artahian@gmail.com>
2024-12-01 15:46:25 -0800

    Fix bfcache issues

--- client/renderApp.tsx
+++ client/renderApp.tsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from 'modelence/client';
 
-export function renderApp({ loadingElement, routesElement }: { loadingElement: React.ReactNode, routesElement: React.ReactNode }) {
+export function renderApp({ loadingElement, routesElement, favicon }: {
+  loadingElement: React.ReactNode,
+  routesElement: React.ReactNode,
+  favicon?: string
+}) {
+  window.addEventListener('unload', () => {
+    // The presence of any 'unload' event handler, even empty,
+    // prevents bfcache in most browsers
+  });
+
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <AppProvider loadingElement={loadingElement}>
@@ -12,7 +21,19 @@ export function renderApp({ loadingElement, routesElement }: { loadingElement: R
         </BrowserRouter>
       </AppProvider>
     </React.StrictMode>
-  )  
+  );
+
+  if (favicon) {
+    const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+    if (!link) {
+      const newLink = document.createElement('link');
+      newLink.rel = 'icon';
+      newLink.href = favicon;
+      document.head.appendChild(newLink);
+    } else {
+      link.href = favicon;
+    }
+  }
 }
 
 export { Routes, Route } from 'react-router-dom';
--- vite.ts
+++ vite.ts
@@ -39,6 +39,11 @@ export function defineViteConfig() {
     server: {
       proxy: {
         '/api': 'http://localhost:4000'
+      },
+      headers: {
+        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+        'Pragma': 'no-cache',
+        'Expires': '0'
       }
     },
     resolve: {
