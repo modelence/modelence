e2ea4d4b6703bcfb998c87cdb3284e18b6bfff6f
artahian <artahian@gmail.com>
2024-12-07 23:50:55 -0800

    Move vite packages into main dependencies

--- vite.ts
+++ vite.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import { createServer, defineConfig } from 'vite';
+import reactPlugin from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 import path from 'path';
 import fs from 'fs';
+import express from 'express';
 
-export function defineViteConfig() {
+export async function initViteServer(app: express.Application, isDev: boolean) {
+  if (isDev) {
+    console.log('Starting Vite dev server...');
+    const vite = await createServer({
+      ...defineConfig(await getConfig()),
+      server: {
+        middlewareMode: true,
+      },
+      root: './src/client'
+    });
+    
+    app.use(vite.middlewares);
+    
+    app.use('*', async (req: express.Request, res: express.Response) => {
+      try {
+        res.sendFile('index.html', { root: './src/client' });
+      } catch (e) {
+        console.error('Error serving index.html:', e);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  } else {
+    app.use(express.static('.modelence/client'));
+    app.get('*', (req, res) => {
+      res.sendFile('index.html', { root: '.modelence/client' });
+    });
+  }
+}
+
+async function getConfig() {
   const appDir = process.cwd();
 
   const eslintConfigFile = [
@@ -16,11 +46,11 @@ export function defineViteConfig() {
     '.eslintrc.yaml'
   ].find(file => fs.existsSync(path.join(appDir, file)));
 
-  const plugins = [react(), modelenceAssetPlugin()];
+  const plugins = [reactPlugin(), modelenceAssetPlugin()];
 
   if (eslintConfigFile) {
     plugins.push(
-      eslint({
+      eslintPlugin({
         failOnError: false,
         include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
         cwd: appDir,
@@ -29,7 +59,7 @@ export function defineViteConfig() {
     );
   }
 
-  return defineConfig({
+  return {
     plugins,
     root: appDir,
     build: {
@@ -52,7 +82,7 @@ export function defineViteConfig() {
       }
 
     }
-  })
+  };
 }
 
 function modelenceAssetPlugin() {
