31af83efbe0e9d88d6609ade44d99a5484c847d9
artahian <artahian@gmail.com>
2024-12-08 21:02:38 -0800

    Expose callMethod to the client

--- client.ts
+++ client.ts
@@ -10,6 +10,6 @@ export const AppProvider = 'useClient' in React
   : OriginalAppProvider;
 
 export { renderApp } from './client/renderApp';
-export { useLoader } from './client/method';
+export { useLoader, callMethod } from './client/method';
 export { useSession } from './client/session';
 export { signupWithPassword, loginWithPassword } from './auth/client';
