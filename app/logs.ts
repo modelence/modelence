d810c63fff3d137fa16875c43e068313c559e2d5
artahian <artahian@gmail.com>
2024-12-11 00:04:24 -0800

    Fix telemetry config

--- app/logs.ts
+++ app/logs.ts
@@ -1,14 +1,16 @@
 import { getLogger } from './metrics';
 
-const isTelemetryEnabled = Boolean(process.env.MODELENCE_SERVICE_ENDPOINT);
-
 export function logInfo(message: string, args: object) {
+  const isTelemetryEnabled = Boolean(process.env.MODELENCE_SERVICE_ENDPOINT);
+
   if (isTelemetryEnabled) {
     getLogger().info(message, args);
   }
 }
 
 export function logError(message: string, args: object) {
+  const isTelemetryEnabled = Boolean(process.env.MODELENCE_SERVICE_ENDPOINT);
+
   if (isTelemetryEnabled) {
     getLogger().error(message, args);
   }
