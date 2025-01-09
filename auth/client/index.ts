81d8965f88a87232af0e4df9d723110b781866ce
artahian <artahian@gmail.com>
2025-01-09 13:59:15 -0800

    Auto-login when signing up

--- auth/client/index.ts
+++ auth/client/index.ts
@@ -7,7 +7,10 @@ type User = {
 };
 
 export async function signupWithPassword({ email, password }: { email: string, password: string }) {
-  return callMethod('_system.user.signupWithPassword', { email, password });
+  await callMethod('_system.user.signupWithPassword', { email, password });
+
+  // TODO: handle auto-login from the signup method itself to avoid a second method call
+  await loginWithPassword({ email, password });
 }
 
 export async function loginWithPassword({ email, password }: { email: string, password: string }) {
