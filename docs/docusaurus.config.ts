50c38aab89595a28edc5bdb19f38951b07909d30
artahian <artahian@gmail.com>
2025-01-27 20:24:18 -0800

    Configure CNAME for docs.modelence.com

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://docs.modelence.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/modelence/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'modelence',
