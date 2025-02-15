80393ab5ea30baaaeae9c41c2d513f99555d3581
artahian <artahian@gmail.com>
2025-02-15 12:21:29 -0800

    Refactor the deploy CLI and add a build step

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -1,19 +1,43 @@
 import { createWriteStream, promises as fs } from 'fs';
+import { execSync } from 'child_process';
 import { join } from 'path';
 import archiver from 'archiver';
 
 export async function deploy(options: {} = {}) {
   const cwd = process.cwd();
   const modelenceDir = join(cwd, '.modelence');
+
   const outputFile = join(modelenceDir, 'bundle.zip');
+
+  await buildProject(modelenceDir);
+
+  await createBundle(modelenceDir, outputFile);
+
+  await uploadBundle(outputFile);
+}
+
+async function buildProject(modelenceDir: string) {
+  console.log('Building project...');
+  
+  try {
+    execSync('npm run build', {
+      cwd: process.cwd(),
+      stdio: 'inherit'
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error('Build failed');
+  }
 
   // Verify .modelence directory exists
   try {
     await fs.access(modelenceDir);
   } catch (error) {
-    throw new Error('Could not find .modelence directory. Please build your project first.');
+    throw new Error('Could not find .modelence directory. Looks like something went wrong during the build.');
   }
+}
 
+async function createBundle(modelenceDir: string, outputFile: string) {
   try {
     await fs.unlink(outputFile);
     console.log('Removed existing bundle');
@@ -58,17 +82,18 @@ export async function deploy(options: {} = {}) {
 
   const stats = await fs.stat(outputFile);
   console.log(`Deployment bundle created at: ${outputFile} (${(stats.size / 1024 / 1024).toFixed(2)} MB)`);
+}
 
+async function uploadBundle(outputFile: string) {
   // TODO: Add authentication
+
   const deploymentId = '677d93fe2cdf304863f3a0f6';
-  const response = await fetch(`http://localhost:3000/api/deployments/${deploymentId}/deploy`, {
+  const response = await fetch(getStudioUrl(`/api/deployments/${deploymentId}/upload`), {
     method: 'POST',
     headers: {
     },
   });
   const { uploadUrl } = await response.json();
-
-  console.log('uploadUrl', uploadUrl);
 
   const fileBuffer = await fs.readFile(outputFile);
   const uploadResponse = await fetch(uploadUrl, {
@@ -84,4 +109,8 @@ export async function deploy(options: {} = {}) {
   }
 
   console.log('Successfully uploaded bundle to Modelence Cloud');
+}
+
+function getStudioUrl(path: string) {
+  return `http://localhost:3000${path}`;
 }
