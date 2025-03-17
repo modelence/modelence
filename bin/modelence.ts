849b3cfe66bfae94057af0226a303db9f2b61405
hello-world2025 <196555573+hello-world2025@users.noreply.github.com>
2025-03-17 13:41:23 +0400

    Create app from scrach

--- bin/modelence.ts
+++ bin/modelence.ts
@@ -5,12 +5,24 @@ import { setup } from './setup';
 import { build } from './build';
 import { deploy } from './deploy';
 import { dev } from './dev';
+import { createApp } from './create-app';
 import { loadEnv } from './config';
 
 const program = new Command()
   .name('modelence')
   .description('Modelence CLI tool')
   .version('0.2.1');
+
+// Commands that don't need config
+program
+  .command('create-app <project-name>')
+  .description('Create a new Modelence application')
+  .option('-t, --template <template-name>', 'Template to use (from examples repository)')
+  .action(async (projectName, options) => {
+    await createApp(projectName, {
+      template: options.template
+    });
+  });
 
 program
   .command('setup')
@@ -21,28 +33,34 @@ program
     await setup(options);
   });
 
-program
+// Commands that need config
+const configCommands = program
+  .command('project')
+  .description('Project management commands');
+
+configCommands
   .command('build')
   .description('Build the application')
   .action(async () => {
+    await loadEnv();
     await build();
   });
 
-program
+configCommands
   .command('deploy')
   .description('Deploy to Modelence Cloud')
   .requiredOption('-e, --env <env>', 'Environment (deployment alias)')
   .action(async (options) => {
+    await loadEnv();
     await deploy(options);
   });
 
-program
+configCommands
   .command('dev')
   .description('Start development server')
   .action(async () => {
+    await loadEnv();
     dev();
   });
 
-loadEnv().then(() => {
-  program.parse(process.argv);
-});
+program.parse(process.argv);
