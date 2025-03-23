906f3e724bbb71048e9c892d1996830a6a28aa66
artahian <artahian@gmail.com>
2025-03-22 17:36:26 -0700

    Replace docusaurus social card

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -67,8 +67,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/modelence-social-card.jpg',
     navbar: {
       title: 'Modelence Docs',
       logo: {
