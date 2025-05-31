aa4208e1ef363802f77bad2ed1f2db575a1c8829
artahian <artahian@gmail.com>
2025-05-31 16:46:39 -0700

    Stop exporting query methods from modelence

--- packages/modelence/src/client.ts
+++ packages/modelence/src/client.ts
@@ -10,6 +10,6 @@ export const AppProvider = 'useClient' in React
   : OriginalAppProvider;
 
 export { renderApp } from './client/renderApp';
-export { modelenceQuery, modelenceMutation, callMethod } from './client/method';
+export { callMethod } from './client/method';
 export { useSession } from './client/session';
 export { signupWithPassword, loginWithPassword, logout } from './auth/client';
