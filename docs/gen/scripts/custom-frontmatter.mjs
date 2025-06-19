762c80f42f2e38a857cf02495f8b72e34d3044ed
artahian <artahian@gmail.com>
2025-06-19 14:28:47 -0700

    Fix doc links

--- docs/gen/scripts/custom-frontmatter.mjs
+++ docs/gen/scripts/custom-frontmatter.mjs
@@ -19,8 +19,12 @@ export function load(app) {
       // Replace occurrences like "(path.mdx" or "path.mdx#" but keep anchors and closing characters
       page.contents = page.contents.replace(/\.mdx(?=[)#])/g, "");
 
+      const baseUrl = page.url.split('/').slice(0, -1).join('/');
+
       // Add a leading dot to internal links to avoid target="_blank"
-      page.contents = page.contents.replace(/\]\(([^/)][^)#]*[^/])\)/g, '](./\$1)');
+      page.contents = page.contents.replace(/\]\(([^/)][^)#]*[^/])\)/g, (match, linkPath) => {
+        return `](/api-reference/${baseUrl}/${linkPath})`;
+      });
     }
   });
 }
