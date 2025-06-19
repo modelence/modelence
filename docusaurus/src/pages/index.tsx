b764416a63816ee908a60b2ec2c387784f1504c9
artahian <artahian@gmail.com>
2025-06-18 22:55:13 -0700

    Delete docusaurus

--- docusaurus/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import {Redirect} from '@docusaurus/router';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  // This will redirect to the new Quick Start homepage
-  return <Redirect to="/quick-start/intro" />;
-}
