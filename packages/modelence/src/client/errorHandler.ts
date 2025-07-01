f4d32aa851cc44c793c954713ba35856ccc1be1f
omegascorp <omegascorp@gmail.com>
2025-07-01 10:29:40 +0400

    lint

--- packages/modelence/src/client/errorHandler.ts
+++ packages/modelence/src/client/errorHandler.ts
@@ -2,12 +2,12 @@ import { getModelenceConfig, setModelenceConfig } from "./config";
 
 export type ErrorHandler = (error: Error, methodName: string) => void;
 
-let defaultErrorHandler: ErrorHandler = (error, methodName) => {
+const defaultErrorHandler: ErrorHandler = (error, methodName) => {
   throw new Error(`Error calling method '${methodName}': ${error.toString()}`);
 };
 
-export function setErrorHandler(handler: ErrorHandler) {
-  setModelenceConfig({ errorHandler: handler });
+export function setErrorHandler(errorHandler: ErrorHandler) {
+  setModelenceConfig({ errorHandler });
 }
 
 export function handleError(error: Error, methodName: string) {
