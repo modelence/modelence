fe4ac131a0271b488be96dd0ee4fb25c63f51827
omegascorp <omegascorp@gmail.com>
2025-07-13 13:28:40 +0400

    remove resend dependency

--- packages/modelence/src/email/email.ts
+++
@@ -1,8 +0,0 @@
-import { loadProvider } from './loadProvider';
-import { EmailData, EmailProviderName } from './types';
-
-export async function sendEmail(providerName: EmailProviderName, data: EmailData) {
-  const emailProvider = await loadProvider(providerName);
-
-  await emailProvider.sendEmail(data);
-}
