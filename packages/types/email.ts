b9ad08479b599e10d5d5f72a7a6e16824d8d4627
omegascorp <omegascorp@gmail.com>
2025-07-14 15:37:38 +0400

    update email type

--- packages/types/email.ts
+++ packages/types/email.ts
@@ -1,8 +1,20 @@
+export type EmailAttachment = {
+  filename: string;
+  content: Buffer | string;
+  contentType: string;
+}
+
 export type EmailPayload = {
   from: string;
   to: string;
   subject: string;
-  html: string;
+  html?: string;
+  text?: string;
+  cc?: string;
+  bcc?: string;
+  replyTo?: string;
+  headers?: Record<string, string>;
+  attachments?: EmailAttachment[];
 }
 
 export interface EmailProvider {
