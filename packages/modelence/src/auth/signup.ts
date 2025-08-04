106ab8340c3160811507e2e769510c94936326cc
omegascorp <omegascorp@gmail.com>
2025-08-04 17:55:25 +0400

    Update sign up email

--- packages/modelence/src/auth/signup.ts
+++ packages/modelence/src/auth/signup.ts
@@ -12,9 +12,9 @@ import { time } from '@/time';
 
 function defaultEmailVerificationTemplate({ name, email, verificationUrl }: { name?: string; email: string; verificationUrl: string }) {
   return `
-    <p>Hello${name ? ` ${name}` : ''},</p>
+    <p>Hi${name ? ` ${name}` : ''},</p>
     <p>Please verify your email address ${email} by clicking the link below:</p>
-    <p><a href="${verificationUrl}">Verify Email</a></p>
+    <p><a href="${verificationUrl}">${verificationUrl}</a></p>
     <p>If you did not request this, please ignore this email.</p>
   `;
 }
