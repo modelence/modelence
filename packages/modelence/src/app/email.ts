16033e8d228323813eb4b139f5476c22a4178432
lightning-sagar <lightningsagar0@gmail.com>
2025-10-03 10:50:25 +0530

    update branch with latest changes

---
+++ packages/modelence/src/app/email.ts
@@ -0,0 +1,9 @@
+import { getEmailConfig } from "@/app/emailConfig";
+import { EmailPayload } from "../types";
+
+export function sendEmail(payload: EmailPayload) {
+  if (!getEmailConfig().provider) {
+    throw new Error('Email provider is not configured, see https://docs.modelence.com/email-configuration for more details.');
+  }
+  return getEmailConfig().provider?.sendEmail(payload);
+}
