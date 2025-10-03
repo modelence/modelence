6ff5a41600441b13f2799fb428b853edb011dbfb
Hayk <omegascorp@gmail.com>
2025-10-03 14:12:06 +0400

    Email docs (#89)

--- packages/modelence/src/app/email.ts
+++ packages/modelence/src/app/email.ts
@@ -3,7 +3,7 @@ import { EmailPayload } from "../types";
 
 export function sendEmail(payload: EmailPayload) {
   if (!getEmailConfig().provider) {
-    throw new Error('Email provider is not configured, see https://docs.modelence.com/email-configuration for more details.');
+    throw new Error('Email provider is not configured, see https://docs.modelence.com/email for more details.');
   }
   return getEmailConfig().provider?.sendEmail(payload);
 }
