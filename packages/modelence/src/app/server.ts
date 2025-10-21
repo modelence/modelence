695283456ab3c7d27a234a575033101333191efa
artahian <artahian@gmail.com>
2025-10-20 20:37:56 -0700

    Increase express request limits

--- packages/modelence/src/app/server.ts
+++ packages/modelence/src/app/server.ts
@@ -43,8 +43,8 @@ export async function startServer(
 ) {
   const app = express();
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '16mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '16mb' }));
   app.use(cookieParser());
 
   app.use(googleAuthRouter());
