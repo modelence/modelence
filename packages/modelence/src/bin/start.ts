1a9f25df795d198f6de08e7f58347e6929c076e8
omegascorp <omegascorp@gmail.com>
2025-12-26 18:03:08 +0400

    fix start path

--- packages/modelence/src/bin/start.ts
+++ packages/modelence/src/bin/start.ts
@@ -1,12 +1,15 @@
-import { getServerPath } from './config';
+import { getBuildPath, getServerPath } from './config';
 import { execSync } from 'child_process';
+import path from 'path';
 
 export function start() {
   console.log('Starting Modelence production server...');
 
   const serverPath = getServerPath();
+  const serverFilename = path.basename(serverPath, path.extname(serverPath));
+  const builtServerPath = getBuildPath(`${serverFilename}.mjs`);
 
-  execSync(`node "${serverPath}"`, {
+  execSync(`node "${builtServerPath}"`, {
     stdio: 'inherit',
     cwd: process.cwd(),
     env: { ...process.env, NODE_ENV: 'production' },
